80d3a4bbec05b55682d2fd02d3ad2959677d90e5
Luis Esteban Cascante Rivera <BanjoCascante@users.noreply.github.com>
2019-02-03 16:09:35 -0600

    Completing antialiasing (#2)
    Removing size of image
    Moving functions from the .cpp file to .h file (Inline)

--- RayTracer/vec3.cpp
+++ RayTracer/vec3.cpp
@@ -13,63 +13,6 @@ vec3::vec3(float x, float y, float z)
     v[2] = z;
 }
 
-
-inline void vec3::makeUnitVector()
-{
-    float l = length();
-    v[0] /= l;
-    v[1] /= l;
-    v[2] /= l;
-}
-
 vec3::~vec3()
 {
-}
-
-inline vec3 & vec3::operator+=(const vec3 & v2)
-{
-    v[0] += v2[0];
-    v[1] += v2[1];
-    v[2] += v2[2];
-    return *this;
-}
-
-inline vec3 & vec3::operator-=(const vec3 & v2)
-{
-    v[0] -= v2[0];
-    v[1] -= v2[1];
-    v[2] -= v2[2];
-    return *this;
-}
-
-inline vec3 & vec3::operator*=(const vec3 & v2)
-{
-    v[0] *= v2[0];
-    v[1] *= v2[1];
-    v[2] *= v2[2];
-    return *this;
-}
-
-inline vec3 & vec3::operator/=(const vec3 & v2)
-{
-    v[0] /= v2[0];
-    v[1] /= v2[1];
-    v[2] /= v2[2];
-    return *this;
-}
-
-inline vec3 & vec3::operator*=(const float & t)
-{
-    v[0] *= t;
-    v[1] *= t;
-    v[2] *= t;
-    return *this;
-}
-
-inline vec3 & vec3::operator/=(const float & t)
-{
-    v[0] /= t;
-    v[1] /= t;
-    v[2] /= t;
-    return *this;
 }
