9fd87fbfd47e0172f172b432fc1e388c78d363c1
Luis Esteban Cascante Rivera <BanjoCascante@users.noreply.github.com>
2019-02-16 23:00:46 -0600

    Chapter 10 Complete (#6)

--- RayTracer/Camera.h
+++ RayTracer/Camera.h
@@ -1,11 +1,13 @@
 #pragma once
-
+#define _USE_MATH_DEFINES
 #include "ray.h"
+#include <math.h>
 class Camera
 {
 public:
     Camera();
     Camera(vec3 o, vec3 llc, vec3 h, vec3 v) : origin{ o }, lowerLeftCorner{ llc }, horizontal{ h }, vertical{ v } {};
+    Camera(vec3 lookFrom, vec3 lookAt, vec3 vecUp, float fov, float aspect);
     ray getRay(float u, float v);
     ~Camera();
 private:
