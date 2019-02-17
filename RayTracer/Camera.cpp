01967a7c97cf4ebd9991159a66d7efa0c055f532
BanjoCascante <lecr0792@hotmail.com>
2019-02-17 17:17:13 -0600

    Random scene generated

--- RayTracer/Camera.cpp
+++ RayTracer/Camera.cpp
@@ -6,9 +6,9 @@ Camera::Camera()
 {
 }
 
-Camera::Camera(vec3 lookFrom, vec3 lookAt, vec3 vecUp, float fov, float aspect)
+Camera::Camera(vec3 lookFrom, vec3 lookAt, vec3 vecUp, float fov, float aspect, float aperture, float focusDist)
 {
-    vec3 u, v, w;
+    lensRadius = aperture / 2;
     float theta{ fov * float(M_PI) / 180.0f };
     float halfHeight{ tanf(theta / 2) };
     float halfWidth{ aspect*halfHeight };
@@ -16,14 +16,16 @@ Camera::Camera(vec3 lookFrom, vec3 lookAt, vec3 vecUp, float fov, float aspect)
     w = unitVector(lookFrom - lookAt);
     u = unitVector(cross(vecUp, w));
     v = cross(w, u);
-    lowerLeftCorner = origin - halfWidth * u - halfHeight * v - w;
-    horizontal = 2 * halfWidth *u;
-    vertical = 2 * halfHeight*v;
+    lowerLeftCorner = origin - halfWidth *focusDist* u - halfHeight *focusDist* v - focusDist*w;
+    horizontal = 2 * halfWidth *focusDist*u;
+    vertical = 2 * halfHeight*focusDist*v;
 
 }
 
-ray Camera::getRay(float u, float v) {
-    return ray(origin, lowerLeftCorner + (u*horizontal) + (v*vertical - origin));
+ray Camera::getRay(float s, float t) {
+    vec3 rd{ lensRadius * randomInUnitDisk() };
+    vec3 offset { u*rd.x() + v * rd.y() };
+    return ray(origin+ offset, lowerLeftCorner + (s*horizontal) + (t*vertical) - origin - offset);
 }
 
 
