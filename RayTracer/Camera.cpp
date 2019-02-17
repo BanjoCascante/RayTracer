1c77ee07b681695cec8c7c017a7847afc9f4954d
BanjoCascante <lecr0792@hotmail.com>
2019-02-16 22:49:07 -0600

    Chapter 10 Complete

--- RayTracer/Camera.cpp
+++ RayTracer/Camera.cpp
@@ -6,6 +6,22 @@ Camera::Camera()
 {
 }
 
+Camera::Camera(vec3 lookFrom, vec3 lookAt, vec3 vecUp, float fov, float aspect)
+{
+    vec3 u, v, w;
+    float theta{ fov * float(M_PI) / 180.0f };
+    float halfHeight{ tanf(theta / 2) };
+    float halfWidth{ aspect*halfHeight };
+    origin = lookFrom;
+    w = unitVector(lookFrom - lookAt);
+    u = unitVector(cross(vecUp, w));
+    v = cross(w, u);
+    lowerLeftCorner = origin - halfWidth * u - halfHeight * v - w;
+    horizontal = 2 * halfWidth *u;
+    vertical = 2 * halfHeight*v;
+
+}
+
 ray Camera::getRay(float u, float v) {
     return ray(origin, lowerLeftCorner + (u*horizontal) + (v*vertical - origin));
 }
