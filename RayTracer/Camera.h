8f152a04513d60341a77f163e17fec9eae868b62
BanjoCascante <lecr0792@hotmail.com>
2019-02-17 17:17:13 -0600

    Random scene generated

--- RayTracer/Camera.h
+++ RayTracer/Camera.h
@@ -2,12 +2,20 @@
 #define _USE_MATH_DEFINES
 #include "ray.h"
 #include <math.h>
+#include <stdlib.h> 
+inline vec3 randomInUnitDisk() {
+    vec3 p;
+    do {
+        p = 2.0f* vec3{ float(rand()) / RAND_MAX,float(rand()) / RAND_MAX, 0.0f } -vec3{ 1.0f,1.0f,0.0f };
+    } while (dot(p, p) >= 1.0f);
+    return p;
+}
 class Camera
 {
 public:
     Camera();
     Camera(vec3 o, vec3 llc, vec3 h, vec3 v) : origin{ o }, lowerLeftCorner{ llc }, horizontal{ h }, vertical{ v } {};
-    Camera(vec3 lookFrom, vec3 lookAt, vec3 vecUp, float fov, float aspect);
+    Camera(vec3 lookFrom, vec3 lookAt, vec3 vecUp, float fov, float aspect, float aperture, float focusDist);
     ray getRay(float u, float v);
     ~Camera();
 private:
@@ -15,5 +23,8 @@ private:
     vec3 lowerLeftCorner;
     vec3 horizontal;
     vec3 vertical;
+    float lensRadius;
+    vec3 u, v, w;
+
 };
 
